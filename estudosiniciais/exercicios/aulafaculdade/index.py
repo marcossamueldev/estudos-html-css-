06499a23009f67ef2fb2aaf59cba43192911d8d0
marcossamueldev <marcossamuelps.dev@gmail.com>
2025-11-23 12:29:26 -0300

    cria index.py
    Adiciona cálculo de área do retângulo
    Define variáveis (nome, idade, cidade) e imprime valores
    Cria loop while com contador de 0 a 4

---
+++ estudosiniciais/exercicios/aulafaculdade/index.py
@@ -0,0 +1,17 @@
+largura = 5
+altura = 3
+area = largura * altura
+print("A area do retangulo é:",area)
+
+nome="marcos"
+idade=25
+cidade="goiania"
+
+print("nome:",nome)
+print("idade:",idade)
+print("cidade:",cidade)
+
+contador=0
+while contador < 5:
+    print("contador é:", contador)
+    contador += 1
